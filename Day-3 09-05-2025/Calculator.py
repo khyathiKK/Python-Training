8fba681732e8eef45d9c775994c1dc0b1ee38be4
Khyathi <khyathikotla@gmail.com>
2025-09-13 21:51:12 +1000

    first prog

---
+++ Day-3 09-05-2025/Calculator.py
@@ -0,0 +1,24 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+def add(num1, num2):
+    return num1 + num2
+
+def sub(num1, num2):
+    return num1 - num2
+
+def mul(num1, num2):
+    return num1 * num2
+
+def div(num1, num2):
+    return num1 / num2
+
+def mod(num1, num2):
+    return num1 % num2
+
+print("Add:",add(num1, num2))
+print("Sub:",sub(num1, num2))
+print("Mul:",mul(num1, num2))
+print("Div:",div(num1, num2))
+print("Mod:",mod(num1, num2))
+
---
+++ Day-3 09-05-2025/practice-1.py
@@ -0,0 +1,20 @@
+x = "awesome"
+
+def myfunc():
+  global x #glabal variable 
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
