8608ca655f5953ad77af9c1044fcaadbf48352fe
Khyathi <khyathikotla@gmail.com>
2025-09-17 05:45:21 +1000

    New file Practice-1 added

---
+++ Day-4 12-09-2025/Practice-1.py
@@ -0,0 +1,11 @@
+#this practice-1 for Git hub
+from datetime import datetime
+from platform import version
+
+#python version checker
+
+print("Python:", version())
+
+#Current date time display
+
+print("Date and time", datetime.now())
