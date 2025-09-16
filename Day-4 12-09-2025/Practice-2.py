885619bf5221111b7b2d36b551ea2d3c7722558d
Khyathi <khyathikotla@gmail.com>
2025-09-17 06:04:41 +1000

    second file added

---
+++ Day-4 12-09-2025/Practice-2.py
@@ -0,0 +1,10 @@
+#text formatter
+
+name = input("Enter your name")
+print(name)
+print("Name in Upper case",name.upper())
+print("Name in lower case",name.lower())
+print("Name in spilt",name.split("khy"))
+print("Number of chars",name.count)
+
+
